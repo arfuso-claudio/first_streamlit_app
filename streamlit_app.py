633384213f40ac99065733836febec555d43e2d6
arfuso-claudio <120491306+arfuso-claudio@users.noreply.github.com>
2023-03-23 15:49:10 +0100

    Update streamlit_app.py
    Add emojis to menu options

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My Parents new Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rokcet Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣  Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rokcet Smoothie')
+streamlit.text('🐔🥑🍞 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')

010edcaa953206e7096d1e4fd70796cf298059d1
arfuso-claudio <120491306+arfuso-claudio@users.noreply.github.com>
2023-03-23 15:51:37 +0100

    Update streamlit_app.py
    Remove extra emoji from menu option 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ streamlit.title('My Parents new Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣  Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rokcet Smoothie')
-streamlit.text('🐔🥑🍞 Hard-Boiled Free-Range Egg')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')

b59eb0226cc58970f2dbafb92e2fa83aa2ec99d4
arfuso-claudio <120491306+arfuso-claudio@users.noreply.github.com>
2023-03-23 15:49:48 +0100

    Update streamlit_app.py
    Add emojis to header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents new Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('🥣  Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rokcet Smoothie')
 streamlit.text('🐔🥑🍞 Hard-Boiled Free-Range Egg')

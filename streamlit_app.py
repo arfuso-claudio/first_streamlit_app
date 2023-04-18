d93760905f34193dd92205ca558d338fd883b3c2
arfuso-claudio <120491306+arfuso-claudio@users.noreply.github.com>
2023-04-18 16:45:57 +0200

    typo change

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,4 +76,4 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add')
 if streamlit.button('Add a fruit to the list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_funtion = insert_row_snowflake(add_my_fruit)
-  streamlit.dataframe(back_from_funtion) 
+  streamlit.dataframe(back_from_funtion)  
